60a2de9bb7a232a5a2908bb14148d152e9bf4082
den3yb <den3yb@gmail.com>
2023-10-08 10:56:35 +0400

    get site

---
+++ laba1.py
@@ -0,0 +1,14 @@
+import os
+import requests 
+from bs4 import BeautifulSoup;
+
+url = "https://otzovik.com/"
+
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.967 YaBrowser/23.9.1.967 Yowser/2.5 Safari/537.36"
+}
+
+req = requests.get(url, headers=headers)
+src = req.text
+print(src)
